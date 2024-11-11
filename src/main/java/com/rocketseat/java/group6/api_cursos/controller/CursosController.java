9eb5641621eb0db9c6679895e3fee7f0f7c2a027
Paulo Flóra <tflora.2004@gmail.com>
2024-11-11 14:17:02 -0300

    substitui Exception por NotFoundException em controller

--- src/main/java/com/rocketseat/java/group6/api_cursos/controller/CursosController.java
+++ src/main/java/com/rocketseat/java/group6/api_cursos/controller/CursosController.java
@@ -6,6 +6,7 @@ import com.rocketseat.java.group6.api_cursos.dto.CursoUpdateResponseDto;
 import com.rocketseat.java.group6.api_cursos.dto.CursosCreateRequestDto;
 import com.rocketseat.java.group6.api_cursos.dto.CursosCreateResponseDto;
 import com.rocketseat.java.group6.api_cursos.service.CursosService;
+import com.rocketseat.java.group6.api_cursos.exceptions.NotFoundException;
 import jakarta.validation.Valid;
 
 import java.util.List;
@@ -35,7 +36,7 @@ public class CursosController {
         try{
             final CursoResponseDto response = service.getById(id);
             return ResponseEntity.ok(response);
-        } catch (final Exception e) {
+        } catch (final NotFoundException e) {
             return ResponseEntity.notFound().build();
         }
     }
@@ -46,7 +47,7 @@ public class CursosController {
         try{
             service.active(id, active);
             return ResponseEntity.noContent().build();
-        } catch (final Exception e) {
+        } catch (final NotFoundException e) {
             return ResponseEntity.notFound().build();
         }
     }
@@ -57,7 +58,7 @@ public class CursosController {
         try{
             final CursoUpdateResponseDto response = service.update(id, request);
             return ResponseEntity.ok(response);
-        } catch (final Exception e) {
+        } catch (final NotFoundException e) {
             return ResponseEntity.notFound().build();
         }
     }
@@ -67,7 +68,7 @@ public class CursosController {
         try{
             service.delete(id);
             return ResponseEntity.noContent().build();
-        } catch (final Exception e) {
+        } catch (final NotFoundException e) {
             return ResponseEntity.notFound().build();
         }
 
@@ -80,7 +81,7 @@ public class CursosController {
         try{
             final List<CursoResponseDto> response = service.getAllWithFilters(name, category);
             return ResponseEntity.ok(response);
-        } catch (final Exception e) {
+        } catch (final NotFoundException e) {
             return ResponseEntity.notFound().build();
         }
     }
